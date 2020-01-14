545a588ba99378a140d80aa9ef6d8b11f8bd576b
Doug Johnson <douglas.j.johnson@gmail.com>
2020-01-14 09:47:47 -0700

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
-  # code goes here
+  attr_reader :name
+  attr_accessor :owner, :mood
+
+  def initialize name, owner
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
 end
